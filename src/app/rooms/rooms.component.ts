7eed7a65e5c53beb8503ed01f0de5af6037815b0
Pablo_Machado <pmmachadov@gmail.com>
2022-12-30 14:00:21 +0100

    Upto 4:30:00

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -13,8 +14,48 @@ export class RoomsComponent {
 
   hideRooms = false;  // Inicialmente se muestran las habitaciones
 
-  toggle() {
-    this.hideRooms = !this.hideRooms; // Cambia el valor de hideRooms a su opuesto
+  rooms: Room = {
+    availableRooms: 20,
+    bookedRooms: 10,
+    totalRooms: 5,
+
+  };
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Single',
+      amenities: 'TV, Wifi, Air Conditioning',
+      price: 100,
+      photos: 'https://www.hilton.com/im/en/rooms/room-1.jpg',
+      checkinTime: new Date(),
+      checkoutTime: new Date(),
+      rating: 2.654568441
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Double',
+      amenities: 'TV, Wifi, Air Conditioning',
+      price: 200,
+      photos: 'https://www.hilton.com/im/en/rooms/room-2.jpg',
+      checkinTime: new Date(),
+      checkoutTime: new Date(),
+      rating: 4.1542
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Triple',
+      amenities: 'TV, Wifi, Air Conditioning',
+      price: 300,
+      photos: 'https://www.hilton.com/im/en/rooms/room-3.jpg',
+      checkinTime: new Date(),
+      checkoutTime: new Date(),
+      rating: 3.0215452
+    },
+  ];
+
+  Toggle() {
+    this.hideRooms = !this.hideRooms;
   }
 
 }
